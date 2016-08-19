f0fe383126ec828e51b58a247fdd42a5752aaf5e
Yannick Adam <yadam@amadeus.com>
2016-08-19 17:35:10 +0200

    ...

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,21 @@ const app = new Koa();
 
 (async ()=> {
 
-    // Connect to Database
-    mongoose.Promise = global.Promise;  // Ensure Mongoose uses native promises
-    await mongoose.connect("mongodb://localhost/project");
+    try {
+        // Connect to Database
+        mongoose.Promise = global.Promise;  // Ensure Mongoose uses native promises
+        await mongoose.connect("mongodb://localhost/project");
 
-    // !!! For demonstration purposes only !!!
-    app.use( (ctx:Koa.Context, next:any) => {
-        ctx.body = "Welcome to Koa!";
-    });
+        // !!! For demonstration purposes only !!!
+        app.use( (ctx:Koa.Context, next:any) => {
+            ctx.body = "Welcome to Koa!";
+        });
 
-    await app.listen(3125);
-    console.log("Server started on port 3125.");
+        await app.listen(3125);
+        console.log("Server started on port 3125.");
+
+    } catch(err) {
+        console.log(err);
+    }
+    
 })();
